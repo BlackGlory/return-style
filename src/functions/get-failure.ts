366dc6cab959b44acb70eb673d9841ed7767c855
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-17 14:50:32 +0000

    chore: fix typo

--- src/functions/get-failure.ts
+++ src/functions/get-failure.ts
@@ -1,6 +1,6 @@
 export function getFailure<E = Error>(
   fn: () => unknown
-): [failed: true, error: E] | [faled: false, error: undefined] {
+): [failed: true, error: E] | [failed: false, error: undefined] {
   try {
     fn()
     return [false, void 0]
